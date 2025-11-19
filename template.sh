0f2505ba60a87a6a0fd47e3203db3944f0f54122
manodya <nadunimanodya@gmail.com>
2025-11-19 20:52:19 +0530

    Inital commit - created the file structure

---
+++ template.sh
@@ -0,0 +1,11 @@
+#create directories
+mkdir -p kb
+
+#create files
+touch kb/overthinking_rules.py
+touch kb/stress_rules.py
+touch kb/left_right_brain_rules.py
+touch facts.py
+touch main.py
+touch inference_engine.py
+touch llm_explainer.py
